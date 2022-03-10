e29e3d91ecc13840faa744593dab8c099941a4d8
asiful086 <asiful086@gmail.com>
2022-03-10 12:41:39 +0600

    auth module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   // imports: [MongooseModule.forRoot('mongodb://localhost/nest-att-auth'), UserModule, RoleModule, PermissionModule],
 
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest-att-auth')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest-att-auth'),
+    AuthModule,
+  ],
 
   // imports: [],
   // controllers: [AppController],
