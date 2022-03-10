4fe1df5eda6cb200b1295b2a0a6de2b5f5055a19
asiful086 <asiful086@gmail.com>
2022-03-10 12:37:33 +0600

    set up database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  // imports: [MongooseModule.forRoot('mongodb://localhost/nest-att-auth'), UserModule, RoleModule, PermissionModule],
+
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest-att-auth')],
+
+  // imports: [],
+  // controllers: [AppController],
+  // providers: [AppService],
 })
 export class AppModule {}
